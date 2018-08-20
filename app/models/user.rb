50ec0fc6e8590ba0c31faf142f365101aade2ec1
natchappo <kaushikhande302@gmail.com>
2018-08-20 09:12:11 +0530

    Make a basic User model (including secure passwords)1

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,10 @@
 class User < ApplicationRecord
 	validates :name, presence: true, length: {maximum: 50}
-	validates :email, presence: true, length: {maximum: 255}
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+	validates :email, presence: true, length: {maximum: 255}, format: {with: VALID_EMAIL_REGEX},uniqueness: {case_sensitive: false}
 	#todo start from format validation
+	has_secure_password
+	validates :password, presence: true, length: {minimum: 8}
 
 
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -6,7 +6,9 @@ class UserTest < ActiveSupport::TestCase
   # end
 
 	def setup
-	@user = User.new(name:"Mike", email: "foobar@new.com")  	
+		@user = User.new(name: "Example User1", email: "user1@example.com",
+                     password: "foobar1", password_confirmation: "foobar1")
+
 	end
 
 	test "should be valid" do
@@ -17,11 +19,11 @@ class UserTest < ActiveSupport::TestCase
 		@user.name = "   "
 		assert_not @user.valid?
 	end
-	
+
 	test "email should be present" do
 	  	@user.email = "   "
 	  	assert_not @user.valid?
-  	end  
+  	end
 
   	test "name should not be too long" do
   		@user.name = "a"* 51
@@ -32,5 +34,29 @@ class UserTest < ActiveSupport::TestCase
   		@user.email = "a"* 244 + "@example.com"
   		assert_not @user.valid?
   	end
+
+		test "email format should be valid" do
+			addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org
+                         first.last@foo.com alice+bob@baz.cn]
+
+			addresses.each do |address|
+				@user.email = address
+				assert @user.valid?, "#{address} should be valid"
+			end
+		end
+
+		test "email address uniqueness" do
+				duplicate_user = @user.dup
+				duplicate_user.email = @user.email.upcase
+				@user.save
+				assert_not duplicate_user.valid?
+		end
+
+		test "email should be saved as lowercase" do
+			mixed_case_email = "fooD@bas.com"
+			@user.email = mixed_case_email
+			@user.save
+			assert_equal mixed_case_email.downcase, @user.reload.email
+		end
 
 end

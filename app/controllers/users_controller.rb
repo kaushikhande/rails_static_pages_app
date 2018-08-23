c3efa0453cf5e6d4875a07870d0d37a43b976969
natchappo <kaushikhande302@gmail.com>
2018-08-23 23:28:01 +0530

    upto gravatar

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,12 @@
 class UsersController < ApplicationController
   def new
   end
+  def show
+    @user = User.find(params[:id])
+    #debugger
+  end
+  private
+  def user_params
+    params.require(:user).permit(:name, :email)
+  end
 end
